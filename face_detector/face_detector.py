982722d39b9579ec5591bcd3353df0d161da6665
Dmitrii Shubin <shubin.dmitrii.n@gmail.com>
2021-10-19 14:28:43 -0400

    refactored main pipeline, added landmark detector

--- face_detector/face_detector.py
+++ face_detector/face_detector.py
@@ -1,6 +1,24 @@
+import onnxruntime as ort
+import cv2
+import numpy as np
 
 class FaceDetector():
 
-    def __init__(self):
+    def __init__(self,model_path):
 
-        print(1)
+        self.model = ort.InferenceSession(model_path)
+
+
+    def run(self,image):
+
+        image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (320, 240))
+        image_mean = np.array([127, 127, 127])
+        image = (image - image_mean) / 128
+        image = np.transpose(image, [2, 0, 1])
+        image = np.expand_dims(image, axis=0)
+        image = image.astype(np.float32)
+
+        input_name = face_detector.get_inputs()[0].name
+        confidences, boxes = face_detector.run(None, {input_name: image})
+        boxes, labels, probs = predict(orig_image.shape[1], orig_image.shape[0], confidences, boxes, threshold)
